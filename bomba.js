8c9c8c4fd8d0c2037604d18dfce412ce29d15943
lucasDeAraujoSilva <lucasaraujosilvamk@gmail.com>
2023-07-27 13:05:02 -0300

    atividade contagem regressiva bomba

---
+++ bomba.js
@@ -0,0 +1,8 @@
+var seg = parseInt(prompt("Quantos segundos para a bomba explodir?"));
+var explodir = 0;
+
+alert("Contagem regressiva");
+for (seg; seg > explodir; seg--) {
+  console.log(seg);
+};
+console.log("BUM");

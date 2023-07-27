8c13425db6eb9d03505a0da4a38d49a08099361b
lucasDeAraujoSilva <lucasaraujosilvamk@gmail.com>
2023-07-27 15:28:03 -0300

    corrige atividade de calculadora infinita

--- calculadora-infinita.js
+++ calculadora-infinita.js
@@ -1,7 +1,9 @@
 function calculadora() {
   var n1 = parseFloat(prompt("Digite o primeiro número"));
   var n2 = parseFloat(prompt("Digite o segundo número"));
-  var operation = prompt("Digite a operação que deseja realizar \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n 5 - Fechar");
+  var operation = prompt(
+    "Digite a operação que deseja realizar \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão"
+  );
 
   while (operation) {
     if (operation === "1") {
@@ -12,13 +14,15 @@ function calculadora() {
       alert("O valor da sua multiplicação é igual a: " + n1 * n2);
     } else if (operation === "4") {
       alert("O valor da sua divisão é igual a: " + n1 / n2);
-    } else if (operation === "5") {
-      alert("Fechar");
-      return;
     } else {
       alert("Essa opção não existe");
     }
-    calculadora();
+    var ask = prompt("Sair?").toLowerCase();
+    if (ask === "sim") {
+        return;
+    } else {
+        calculadora();
+    }
   }
 }
 

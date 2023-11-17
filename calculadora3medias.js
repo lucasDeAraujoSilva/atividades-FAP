31bf53b6c6652c954457748b9ef235f1f58c0f7f
lucasDeAraujoSilva <lucasaraujosilvamk@gmail.com>
2023-11-17 18:19:55 -0300

    calculando 3 medias em js

---
+++ calculadora3medias.js
@@ -0,0 +1,7 @@
+var nota1 = parseFloat(prompt("Digite a terceira nota"));
+var nota2 = parseFloat(prompt("Digite a segunda nota"));
+var nota3 = parseFloat(prompt("Digite a terceira nota"));
+
+var media = (nota1 + nota2 + nota3) / 3;
+
+console.log("A sua média é = " + media)

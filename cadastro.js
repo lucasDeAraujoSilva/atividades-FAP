94c0bf4123a9740fc9599ec946d624a2022b1053
lucasDeAraujoSilva <lucasaraujosilvamk@gmail.com>
2023-11-17 18:08:42 -0300

    cadastro utilizando Map

---
+++ cadastro.js
@@ -0,0 +1,8 @@
+
+const cadastro = new Map()
+    cadastro.set("nome" ,"Lucas") //string
+    cadastro.set("salario", "R$ 1.300,00") //string
+    cadastro.set("idade" , 25) //number
+    cadastro.set("diploma" , true); //boolean
+
+    console.log(cadastro);

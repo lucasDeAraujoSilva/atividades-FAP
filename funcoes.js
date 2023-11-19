fafef293422fc4bf6c96352d4bcc853024a106b0
lucasDeAraujoSilva <lucasaraujosilvamk@gmail.com>
2023-11-18 22:58:52 -0300

    atividade mostrando tipos de funcoes

---
+++ funcoes.js
@@ -0,0 +1,23 @@
+function messenger(){
+return "Olá!!"
+};
+console.log(messenger());
+
+
+
+
+
+function aluno (name, lastName){
+    return name + lastName;
+};
+ 
+console.log(aluno("Lucas" , " Araújo"))
+
+
+
+
+
+const person = (name, lastName) => {
+    return name + lastName;
+};
+console.log(person("Lucas" , " Araújo"))

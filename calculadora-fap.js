e4e66c244938d2d7083c426a718a94779cffa444
lucasDeAraujoSilva <lucasaraujosilvamk@gmail.com>
2023-11-18 22:42:23 -0300

    atualizacao para calcular resto

--- calculadora-fap.js
+++ calculadora-fap.js
@@ -1,18 +1,18 @@
 var n1 = parseFloat(prompt("Digite o primeiro número"));
 var n2 = parseFloat(prompt("Digite o segundo número"));
-var operation = prompt("Digite a operação que deseja realizar \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão");
+var operation = prompt("Digite a operação que deseja realizar \n + = Soma \n - = Subtração \n * = Multiplicação \n / = Divisão");
 
 function calculadora(v1, v2, op) {
-  if (op === "1") {
+  if (op === "+") {
     return "O valor da sua soma é igual a: " + (v1 + v2);
-  } else if (op === "2") {
+  } else if (op === "-") {
     return "O valor da sua subtração é igual a: " + (v1 - v2);
-  } else if (op === "3") {
+  } else if (op === "*") {
     return "O valor da sua multiplicação é igual a: " + v1 * v2;
-  } else if (op === "4") {
-    return "O valor da sua divisão é igual a: " + v1 / v2;
+  } else if (op === "/") {
+    return "O valor da sua divisão é igual a: " + parseInt(v1 / v2) + ". O resto da sua divisão é: " + v1 % v2;
   } else {
-    return 0;
+    return "Por favor, verifique as solicitações nos campos e vamos recomeçar! ;)";
   }
 }
 console.log(calculadora(n1, n2, operation));
